e951b69670f06875662b92b42075b935939dba29
Boonsrp029 <49550816+Boonsrp029@users.noreply.github.com>
2019-04-25 11:30:58 +0700

    โค้ดอัพเดท(แก้บัค)

--- project.py
+++ project.py
@@ -1,35 +1,32 @@
-import pygame,time
-from pygame.locals import *
+import pygame
 pygame.init()
 pygame.display.set_caption('2 pic 1 word')
 #MENU_SCREEN
 screen = pygame.display.set_mode((1280, 720))
-bg = pygame.image.load('Picture/bg.gif')
+bg = pygame.image.load('Picture/bg.png')
 bg = pygame.transform.scale(bg, (1280, 720))
 bg = bg.convert()
 clock = pygame.time.Clock()
-logo = pygame.image.load('Picture/logo.jpg')
-font = pygame.font.SysFont('Arial',30)
-word = font.render('FUCK                            YOU',True,(0,0,0))
 icon_game = pygame.image.load('Picture/logo.jpg')
 icon_game = pygame.transform.scale(icon_game,(100,100))
 #SOUND
 menu_sound = False
 play_sound = False
-game_over_sound = False
 #BUTTON
 start = pygame.image.load('Picture/Button/start.png')
-start = pygame.transform.scale(start,(170,90))
+start = pygame.transform.scale(start,(200,100))
 start_click = pygame.image.load('Picture/Button/start_click.png')
-start_click = pygame.transform.scale(start_click,(170,90))
+start_click = pygame.transform.scale(start_click,(200,100))
 exit_n = pygame.image.load('Picture/Button/exit.png')
+exit_n = pygame.transform.scale(exit_n,(200,100))
 exit_click = pygame.image.load('Picture/Button/exit_click.png')
+exit_click = pygame.transform.scale(exit_click,(200,100))
 #GAME_PIC
 pic1 = ['sun.jpg','egg.jpg','butter.jpg','sun.jpg','rain.jpg','pan.jpg','ear.png',
-        'snow.jpg','cat.jpg','wall.jpg','book.jpg','cheese.jpg','bird.jpg','skate.jpg',
+        'snow.jpg','cat.jpg','wall.jpg','book.png','cheese.jpg','bird.jpg','skate.jpg',
         'jelly.jpg','basket.jpg','wheel.jpg','lady.jpg','hand.jpg','rain.jpg','pill.jpg',
         'home.jpg','down.jpg','sun.jpg','arm.jpg','light.jpg','tea.jpg','scare.jpg',
-        'cork.jpg','book.jpg','tooth.png','water.jpg','road.jpg','pig.jpeg','dragon.jpeg',
+        'cork.jpg','book.png','tooth.png','water.jpg','road.jpg','pig.png','dragon.png',
         'grape.jpg','lip.jpg','air.jpg','dish.jpg','cup.jpg','hand.jpg','star.png',
         'cow.jpg','sheet.jpg','back.jpg','honey.jpg','bag.jpg','hose.jpg','pop.jpg',
         'drum.jpg']
@@ -46,14 +43,10 @@ bg_game = pygame.transform.scale(bg_game,(1280,720))
 #ANSWER#
 answer = []
 integer = ['1','2','3','4','5','6','7','8','9','0']
-
-for a in range(len(pic1)):
-    if pic1[a][-1] in integer:
-        pic1[a] = pic1[a][:-1]
-    if pic2[a][-1] in integer:
-        pic2[a] = pic2[a][:-1]
+for a in range(len(pic1)): 
     res = pic1[a][:-4] + pic2[a][:-4]
-    answer.append(res)
+    ans = '.'.join(str(ord(c)) for c in res)
+    answer.append(ans)
 
 def text_objects(text,font,color=None):
     if not color:
@@ -76,24 +69,25 @@ def game():
     show_input = ''
     set_letter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     set_keyboard = [pygame.K_a,pygame.K_b,pygame.K_c,pygame.K_d,pygame.K_e,pygame.K_f,pygame.K_g,pygame.K_h,pygame.K_i,pygame.K_j,pygame.K_k,pygame.K_l,pygame.K_m,pygame.K_n,
-                pygame.K_o,pygame.K_p,pygame.K_q,pygame.K_r,pygame.K_s,pygame.K_t,pygame.K_u,pygame.K_v,pygame.K_w,pygame.K_x,pygame.K_y,pygame.K_z,pygame.K_BACKSPACE,pygame.K_SPACE,13,pygame.K_RETURN]
+                pygame.K_o,pygame.K_p,pygame.K_q,pygame.K_r,pygame.K_s,pygame.K_t,pygame.K_u,pygame.K_v,pygame.K_w,pygame.K_x,pygame.K_y,pygame.K_z,pygame.K_BACKSPACE,pygame.K_SPACE,13]
     menu_sound.stop()
-    global play_sound,game_over_sound
+    global play_sound
     if not play_sound:
         play_sound = pygame.mixer.Sound('Sound/Chibi Ninja.wav')
-        play_sound.set_volume(.5)
+        play_sound.set_volume(.2)
         play_sound.play(loops=-1)
     #GAME
     while True:
-        screen.fill(0)
-        screen.blit(bg_game,(0,0))
-        picture1 = pygame.image.load('Picture/pics1/'+pic1[i])
-        picture2 = pygame.image.load('Picture/pics2/'+pic2[i])
-        picture1 = pygame.transform.scale(picture1,(500,300))
-        picture2 = pygame.transform.scale(picture2,(500,300))
-        screen.blit(picture1,(300,200))
-        screen.blit(picture2,(900,200))
         for event in pygame.event.get():
+            pygame.display.update()
+            screen.fill(0)
+            screen.blit(bg_game,(0,0))
+            picture1 = pygame.image.load('Picture/pics1/'+pic1[i])
+            picture2 = pygame.image.load('Picture/pics2/'+pic2[i])
+            picture1 = pygame.transform.scale(picture1,(200,200))
+            picture2 = pygame.transform.scale(picture2,(200,200))
+            screen.blit(picture1,(300,200))
+            screen.blit(picture2,(900,200))
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
@@ -106,14 +100,21 @@ def game():
                 elif event.key == pygame.K_SPACE or event.key == 13:
                     #CORRECT
                     if answer[i] == input_:
+                        i += 1
                         if input_ != '':
                             output = input_.split('.')
                             for a in output:
                                 a = int(a) - 97
                                 show_input += set_letter[a]
+                                correct_sound = pygame.mixer.Sound('Sound/correct.wav')
+                                correct_sound.set_volume(.5)
+                                correct_sound.play()
                                 show_input = ''
                         input_ = ''
                     else:
+                        wrong_sound = pygame.mixer.Sound('Sound/wrong.wav')
+                        wrong_sound.set_volume(.5)
+                        wrong_sound.play()
                         pygame.display.update()
                         input_ = ''
                 else:
@@ -179,10 +180,8 @@ def menu():
             screen.blit(bg, (0, 0))
             pygame.time.delay(100)
         screen.blit(bg,(0,0))
-        screen.blit(icon_game,(600,100))
-        screen.blit(word,(500,100))
-        #button(exit_n,exit_click,570,360,100,100,'exit')
-        button(start,start_click,570,250,100,100,'start')
+        button(exit_n,exit_click,80,360,100,100,'exit')
+        button(start,start_click,80,250,100,100,'start')
         pygame.display.update()
         clock.tick(30)
 pygame.init()
